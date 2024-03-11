0a528e23ff5272b9e5849cce4464e7bcf25df19b
kaileyhh <kaileyhua@gmail.com>
2024-03-11 16:55:11 -0400

    dictionary from classifier, bash script loop in progress

--- classifier/classification_16s/command_line_classification_script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# command_line_classification_script
-# Configure paths
-# RDPToolsDir=/mnt/research/rdp/public/RDPTools # Path to RDPTools on MSU's HPCC
-RDPToolsDir=/usr/local/RDPTools # Path to RDPTools on my local installation
-
-# Classifiy trimmed sequences. Trimmed sequences are in sub-folder trimmed_seqs
-java -Xmx2g -jar $RDPToolsDir/classifier.jar classify --gene 16srrna -c 0.5 --format fixrank --hier_outfile hier_file.txt --outputFile classified.txt trimmed_seqs/*.fastq
