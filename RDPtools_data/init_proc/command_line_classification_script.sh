ee1ea9ffb4a4fe9bc7f6d8cef684879d47e2e286
kaileyhh <kaileyhua@gmail.com>
2024-03-11 15:39:18 -0400

    RDP BLAST WORKS!!!!

--- RDPtools_data/init_proc/command_line_classification_script.sh
+++ RDPtools_data/init_proc/command_line_classification_script.sh
@@ -2,7 +2,8 @@
 # command_line_classification_script
 # Configure paths
 # RDPToolsDir=/mnt/research/rdp/public/RDPTools # Path to RDPTools on MSU's HPCC
-RDPToolsDir=/~/Desktop/courses-s24/microbiome-networks # Path to RDPTools on my local installation
+RDPToolsDir=~/Desktop/courses-s24/microbiome-networks # Path to RDPTools on my local installation
+ClassifierDir=~/rdp_classifier_2.14/dist
 
 # Classifiy trimmed sequences. Trimmed sequences are in sub-folder trimmed_seqs
-java -Xmx2g -jar $RDPToolsDir/classifier.jar classify --gene 16srrna -c 0.5 --format fixrank --hier_outfile hier_file.txt --outputFile classified.txt ERR4775526.fastq
+java -Xmx2g -jar $ClassifierDir/classifier.jar classify --gene 16srrna -c 0.5 --format fixrank --hier_outfile hier_file.txt --outputFile classified.txt ERR4775526.fastq
